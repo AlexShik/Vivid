a756f32f9fd21f49ede586a307ee094bafc20bea
Alexander Shinkarenko <ashinkarenko@smedialink.com>
2019-05-16 15:22:43 +0300

    Added iOS Scheme

---
+++ VividDemo/Pods/Vivid-iOS/Vivid_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Vivid_iOS.h
+//  Vivid-iOS
+//
+//  Created by Alexander Shinkarenko on 16/05/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vivid_iOS.
+FOUNDATION_EXPORT double Vivid_iOSVersionNumber;
+
+//! Project version string for Vivid_iOS.
+FOUNDATION_EXPORT const unsigned char Vivid_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vivid_iOS/PublicHeader.h>
+
+
